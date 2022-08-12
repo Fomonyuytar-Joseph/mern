67173f78e188148fbb6600252b0a3406daf17e83
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2022-08-12 10:35:27 +0100

    fetch data from database

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { useState ,useEffect } from 'react'
+import WorkoutDetails from '../components/WorkoutDetails'
+
 
 
 
 function Home() {
+
+    const [workouts ,setWorkouts] = useState(null)
+
+    useEffect(() => {
+      const fetchWorkouts= async ()=>{
+        const response= await fetch('/api/workouts')
+        const json = await response.json()
+
+        if(response.ok){
+
+            setWorkouts(json)
+
+        }
+      }
+
+      fetchWorkouts()
+    
+    }, [])
+    
   return (
-    <div>Home</div>
+    <div className='home'>
+        <div className='workouts'>
+           {workouts && workouts.map( (workout)=>(
+            <WorkoutDetails key={workout._id} workout={workout}/>
+        
+           )) }
+        </div>
+    </div>
   )
 }
 
